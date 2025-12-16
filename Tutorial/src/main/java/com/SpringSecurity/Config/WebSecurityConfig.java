ddb8616f1ceaaaf9f5d0fa62f0f0430a68bd22fd
Pranjal Singh <155311442+06Pranjal@users.noreply.github.com>
2025-12-16 19:47:05 +0530

    BCrypt Enoder Implementation

--- Tutorial/src/main/java/com/SpringSecurity/Config/WebSecurityConfig.java
+++ Tutorial/src/main/java/com/SpringSecurity/Config/WebSecurityConfig.java
@@ -3,10 +3,9 @@ package com.SpringSecurity.Config;
 import com.SpringSecurity.security.JwtAuthFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,7 +16,7 @@ public class WebSecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance(); // TEMP ONLY
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
